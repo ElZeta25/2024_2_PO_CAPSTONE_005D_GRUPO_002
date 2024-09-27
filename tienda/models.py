acc7a22425c86697e4875a343cae5148b5e3e286
Martin <mart.godoy@duocuc.cl>
2024-09-27 10:11:18 -0300

    27-09-24

--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 class Producto(models.Model):
@@ -6,6 +7,31 @@ class Producto(models.Model):
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
     imagen = models.ImageField(upload_to='imagenes/', blank=True, null=True)
+    def __str__(self):
+        return self.nombre
+
+class OrderItem(models.Model):
+    item= models.ForeignKey(Producto,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nombre
+
+
+class Orden(models.Model):
+    user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    items=models.ManyToManyField(OrderItem)
+    start_date=models.DateTimeField(auto_now_add=True)
+    ordened_date=models.DateTimeField()
+    ordened = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.user.username
+
+
+
+
+
+
+
+
--- tienda/urls.py
+++ tienda/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.conf import settings
+from django.urls import path, include
 from . import views
+from django.contrib import admin
+from .views import Producto
+
+app_name='core'
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
 ]
